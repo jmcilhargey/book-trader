f9246f2e0b4ff73a40b7362180250e55bd4ab8a9
jmcilhargey <jmcilhargey@gmail.com>
2016-11-24 17:50:51 -0800

    Added route for trade detail that includes id as param so user can see book details and select a book to offer

--- client/source/app.js
+++ client/source/app.js
@@ -3,24 +3,29 @@
 import "babel-polyfill";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Router, Route, browserHistory } from "react-router";
+import { Router, IndexRoute, Route, browserHistory } from "react-router";
 
 import App from "./views/app";
+import Home from "./views/home/home";
 import Register from "./views/register/register";
 import Login from "./views/login/login";
 import Search from "./views/search/search";
 import Library from "./views/library/library";
 import Settings from "./views/settings/settings";
+import Trade from "./views/trade/trade";
 import "style!./main.css";
 
 ReactDOM.render(
     <Router history={ browserHistory }>
         <Route path="/" component={ App } >
-          <Route path="/search" component={ Library } ></Route>
+          <IndexRoute component={ Home } />
+          <Route path="/browse" component={ Library } ></Route>
+          <Route path="/browse/:id" component={ Trade } ></Route>
           <Route path="/register" component={ Register } ></Route>
           <Route path="/login" component={ Login } ></Route>
-          <Route path="/account" component={ Search } ></Route>
+          <Route path="/search" component={ Search } ></Route>
           <Route path="/settings" component={ Settings } ></Route>
         </Route>
+        <Route path="*" component={ App } />
     </Router>
     , document.getElementById("app"));
